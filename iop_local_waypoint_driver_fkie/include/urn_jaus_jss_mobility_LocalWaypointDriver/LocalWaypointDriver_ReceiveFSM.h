3eee34955fd0ce83f7001b9025a812c2f7ca4975
Alexander Tiderko <Alexander.Tiderko@fkie.fraunhofer.de>
2018-05-03 16:51:45 +0200

    added local waypoint driver

---
+++ iop_local_waypoint_driver_fkie/include/urn_jaus_jss_mobility_LocalWaypointDriver/LocalWaypointDriver_ReceiveFSM.h
@@ -0,0 +1,81 @@
+
+
+#ifndef LOCALWAYPOINTDRIVER_RECEIVEFSM_H
+#define LOCALWAYPOINTDRIVER_RECEIVEFSM_H
+
+#include "JausUtils.h"
+#include "InternalEvents/InternalEventHandler.h"
+#include "Transport/JausTransport.h"
+#include "JTSStateMachine.h"
+#include "urn_jaus_jss_mobility_LocalWaypointDriver/Messages/MessageSet.h"
+#include "urn_jaus_jss_mobility_LocalWaypointDriver/InternalEvents/InternalEventsSet.h"
+
+#include "InternalEvents/Receive.h"
+#include "InternalEvents/Send.h"
+
+#include "urn_jaus_jss_core_Transport/Transport_ReceiveFSM.h"
+#include "urn_jaus_jss_core_Events/Events_ReceiveFSM.h"
+#include "urn_jaus_jss_core_AccessControl/AccessControl_ReceiveFSM.h"
+#include "urn_jaus_jss_core_Management/Management_ReceiveFSM.h"
+
+#include <ros/ros.h>
+#include <nav_msgs/Path.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <std_msgs/Float32.h>
+#include <std_msgs/Bool.h>
+
+#include "LocalWaypointDriver_ReceiveFSM_sm.h"
+
+namespace urn_jaus_jss_mobility_LocalWaypointDriver
+{
+
+class DllExport LocalWaypointDriver_ReceiveFSM : public JTS::StateMachine
+{
+public:
+	LocalWaypointDriver_ReceiveFSM(urn_jaus_jss_core_Transport::Transport_ReceiveFSM* pTransport_ReceiveFSM, urn_jaus_jss_core_Events::Events_ReceiveFSM* pEvents_ReceiveFSM, urn_jaus_jss_core_AccessControl::AccessControl_ReceiveFSM* pAccessControl_ReceiveFSM, urn_jaus_jss_core_Management::Management_ReceiveFSM* pManagement_ReceiveFSM);
+	virtual ~LocalWaypointDriver_ReceiveFSM();
+
+	/// Handle notifications on parent state changes
+	virtual void setupNotifications();
+
+	/// Action Methods
+	virtual void resetTravelSpeedAction();
+	virtual void sendReportLocalWaypointAction(QueryLocalWaypoint msg, Receive::Body::ReceiveRec transportData);
+	virtual void sendReportTravelSpeedAction(QueryTravelSpeed msg, Receive::Body::ReceiveRec transportData);
+	virtual void setTravelSpeedAction(SetTravelSpeed msg, Receive::Body::ReceiveRec transportData);
+	virtual void setWaypointAction(SetLocalWaypoint msg, Receive::Body::ReceiveRec transportData);
+
+
+	/// Guard Methods
+	virtual bool isControllingClient(Receive::Body::ReceiveRec transportData);
+	virtual bool waypointExists(QueryLocalWaypoint msg);
+	virtual bool waypointExists(SetTravelSpeed msg);
+
+
+	LocalWaypointDriver_ReceiveFSMContext *context;
+
+protected:
+
+    /// References to parent FSMs
+	urn_jaus_jss_core_Transport::Transport_ReceiveFSM* pTransport_ReceiveFSM;
+	urn_jaus_jss_core_Events::Events_ReceiveFSM* pEvents_ReceiveFSM;
+	urn_jaus_jss_core_AccessControl::AccessControl_ReceiveFSM* pAccessControl_ReceiveFSM;
+	urn_jaus_jss_core_Management::Management_ReceiveFSM* pManagement_ReceiveFSM;
+
+	ros::Publisher p_pub_path;
+	ros::Publisher p_pub_pose;
+	ros::Publisher p_pub_tv_max;
+	ros::Subscriber p_sub_finished;
+	float p_travel_speed;
+	float p_tv_max;
+	std::string p_tf_frame_robot;
+	ReportLocalWaypoint p_current_waypoint;
+
+	void pStop();
+	void pRosFinished(const std_msgs::Bool::ConstPtr& state);
+
+};
+
+};
+
+#endif // LOCALWAYPOINTDRIVER_RECEIVEFSM_H
